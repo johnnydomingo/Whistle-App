14de4032b7a3af6acc6e2881f635136a6283f136
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-10-23 12:05:37 -0400

    I was looking for the Easter Egg in the repo regarding the first commit since I missed that last time

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Home from './components/Home.js';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav>
+      <h1>Whistle!</h1>
+      </nav>
+      <Home />
+    </div>
+  );
+}
+document.title = "Whistle!";
+export default App;
