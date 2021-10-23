91eab1e61cd9815c8df063ff3d172c26e34dd6c2
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-10-23 13:46:01 -0400

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,21 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
+import Feed from './components/Feed.js';
 import Home from './components/Home.js';
 import './App.css';
 
-const API_URL = 'https://api.airtable.com/v0/apph3EMCub9HXZcv5/Table%201?api_key=keyjukEQxfYibCtcU';
+
 
 function App() {
-  const [whistles, setWhistles] = useState([]);
-  const [toggleFetch, setToggleFetch] = useState(true);
-
-    useEffect(() => {
-      const getWhistles = async () => {
-        const resp = await axios.get(API_URL);
-        console.log(resp.data);
-        setWhistles(resp.data.records)
-      }
-      getWhistles();
-    }, [toggleFetch]);
-
-  document.title = "Whistle!";
   
 
 
+  document.title = "Whistle!";
+  
     return (
       <div>
-       
-        <Home />
-
+        
       </div>
     );
 }
--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,25 +1,11 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 
-
-const API_URL = 'https://api.airtable.com/v0/apph3EMCub9HXZcv5/Table%201?api_key=keyjukEQxfYibCtcU';
-
-const Feed = () => {
-  const [whistles, setWhistles] = useState([]);
-  const [toggleFetch, setToggleFetch] = useState(true);
-
-  useEffect(() => {
-    const getWhistles = async () => {
-      const resp = await axios.get(API_URL);
-      console.log(resp.data);
-    }
-    getWhistles();
-  })
-
-
+const Feed = ({ whistleData }) => {
 
   return (
     <div>
+      <h4>{whistleData?.fields.username}</h4>
+      <p>{whistleData?.fields.post}</p>
+      <p>{whistleData?.fields.date}</p>
 
     </div>
   )
