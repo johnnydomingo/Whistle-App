245e8c8e255d2919f8213593e0553b19c02bf525
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-10-26 13:32:29 -0400

    Updated

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,14 +1,13 @@
 const About = () => {
   return (
     <div>
-      <h1>About Whistle</h1>
-      <h4>What's all the whistle about?</h4>
+      <h5>What's all the whistle about?</h5>
       <div className='about-me'>
         <article>
           Here at Whistle, we aim to create a space for users to share anything on their mind in an instant. While most of our site is still under construction, users are able to make a simple post and view posts from other users in an up-to-date feed. The developers and management of Whistle thank you for being an early user and we are so excited to continue to create a great experience for you!
         </article>
-        <div className='empty'></div>
       </div>
+      <div className='empty'></div>
     </div>
   )
 }
