b93ea394407105e5b30c3e925e6d4e77882594ae
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-10-26 10:01:50 -0400

    Updated

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -27,16 +27,10 @@ const Home = ({ setCurrentUser, toggleFetch, setToggleFetch}) => {
       <div>
         <h1>Welcome to Whistle!</h1>
       </div>
-      <div style={{
-        position: 'relative', width: '100%', height: '0', paddingTop: '100.0000%',
-        paddingBottom: '48px', boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)', marginTop: '1.6em', marginBottom: '0.9em', overflow: 'hidden',
-        borderRadius: '8px', willChange: 'transform'
-      }}>
-  <iframe loading='lazy' style={{position: 'absolute', width: '100%', height: '100%', top: '0', left: '0', border: 'none', padding: 0;margin: 0;"
-    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEt7a0dka8&#x2F;watch?embed">
-  </iframe>
-</div>
-<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEt7a0dka8&#x2F;watch?utm_content=DAEt7a0dka8&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Peach Speech Bubble Animals &amp; Pets Logo</a> by Johnny Domingo
+      <div className='logo'>
+        <img src='https://i.imgur.com/4whjMuV.png'></img>
+      </div>
+
       
       {/* <form onSubmit={handleSubmit}>
         <label htmlFor='username'></label>
