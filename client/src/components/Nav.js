33f2a75f203b258a2a490cf1fec726d5fc273aa8
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-10-26 20:48:30 -0400

    Updated

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,49 +4,49 @@ import { useState } from 'react';
 
 const Nav = () => {
   const [click, setClick] = useState(true);
+  // const [menu, setMenu] = useState(false);
 
   const handleClick = () => setClick(!click);
   
-
-          
-
+  // const showMenu = () => {
+  //   if (window.innerWidth > 501) {
+  //     setMenu(true);
+  //   } else {
+  //     setMenu(true);
+  //   }
+  // };
   return (
     <div>
       <nav className='navbar'>
         <div
           className='brand-name'>Whistle
         </div>
-        { click ? 
+          {click ? window.innerWidth > 501 &&
+          // click is true OR window is larger than 500px
           <div href='#' className='hamburger' onClick={handleClick}>
-          <span className='line'></span>
-          <span className='line'></span>
-          <span className='line'></span>
-          </div> :
-            <div className='newMenu'>
-              <Link to='/home'>Home</Link>
-              <Link to='/feed'>Feed</Link>
-              <Link to='/about'>About</Link>
-            </div> }
-        {/* refer back to handle click -  */}
-        <div className='links'>
-          <ul>
-            <li>
-              <a>
-                <Link to='/home'>Home</Link>
-              </a>
-            </li>
-            <li>
-              <a>
-                <Link to='/feed'>Feed</Link>
-              </a>
-            </li>
-            <li>
-              <a>
-                <Link to='/about'>About</Link>
-              </a>
-            </li>
-          </ul>
-        </div>
+            <span className='line'></span>
+            <span className='line'></span>
+            <span className='line'></span>
+          </div> : <div className='links'>
+            {/* {menu && showMenu} */}
+            <ul>
+              <li>
+                <a>
+                  <Link to='/home'>Home</Link>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <Link to='/feed'>Feed</Link>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <Link to='/about'>About</Link>
+                </a>
+              </li>
+            </ul>
+          </div> }
       </nav>
     </div>
   )
